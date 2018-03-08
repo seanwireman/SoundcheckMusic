e8e6aec409fc1a769366e9c14dbcbbe1e923a7ba
seanwireman <sean.wireman@altec.com>
2018-03-08 09:32:53 -0500

    PythonSendEmail2.7Version

--- sendemail.py
+++ sendemail.py
@@ -1,9 +1,21 @@
 import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+ 
+ 
+fromaddr = "soundcheckmusicco@gmail.com"
+toaddr = "soundcheckmusicco@gmail.com, seanwireman@gmail.com"
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = "SUBJECT OF THE MAIL"
+ 
+body = "YOUR MESSAGE HERE"
+msg.attach(MIMEText(body, 'plain'))
  
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login("soundcheckmusicco@gmail.com", "uQQtBN5Ra@Hs%2ZgCPwX")
- 
-msg = "YOUR MESSAGE!"
-server.sendmail("seanwireman@hotmail.com", "seanwireman@gmail.com", msg)
+server.login(fromaddr, "uQQtBN5Ra@Hs%2ZgCPwX")
+text = msg.as_string()
+server.sendmail(fromaddr, toaddr, text)
 server.quit()
