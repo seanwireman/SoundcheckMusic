75deadf7b84433dc499b5eb090c77d87a0cabed5
seanwireman <sean.wireman@altec.com>
2018-04-26 08:43:04 -0400

    Added email-config File

---
+++ public_html/email-config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+//SERVER INFO
+$host = "smtp.gmail.com";
+$port = 587;
+
+
+//LOGIN INFO
+$email = "seanwireman@gmail.com";
+$pass = "PnLT3889HtFysfM*Gm9F8";
+
+
+//EMAIL FORMATTING
+$from = "SoundCheck Music Contact Form";
+$to = ["seanwireman@gmail.com", "sean.wireman@altec.com"];
+
+?>
